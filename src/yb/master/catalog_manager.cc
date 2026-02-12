b463c6127617042038a8c4ccaa2b456680e4ee60
Keisuke Umegaki <keisuke.umegaki@shopify.com>
2026-02-12 21:51:36 +0900

    YSQL: Add `{active_ssts,wals}_size` to `yb_tablet_metadata`

--- src/yb/master/catalog_manager.cc
+++ src/yb/master/catalog_manager.cc
@@ -14269,6 +14269,8 @@ Status CatalogManager::GetTabletsMetadata(const GetTabletsMetadataRequestPB* req
       }
 
       auto replica_locations = tablet->GetReplicaLocations();
+      uint64_t leader_sst_files_size = 0;
+      uint64_t leader_wal_files_size = 0;
       for (const auto& [ts_uuid, replica] : *replica_locations) {
         // Get tablet server info to extract IP address and PostgreSQL port
         auto ts_desc_result = master_->ts_manager()->LookupTSByUUID(ts_uuid);
@@ -14295,8 +14297,12 @@ Status CatalogManager::GetTabletsMetadata(const GetTabletsMetadataRequestPB* req
 
           if (replica.role == PeerRole::LEADER) {
             leader_address = server_address;
+            leader_sst_files_size = replica.drive_info.sst_files_size;
+            leader_wal_files_size = replica.drive_info.wal_files_size;
           } else {
             tablet_metadata->add_replicas(server_address);
+            tablet_metadata->add_replicas_sst_files_size(replica.drive_info.sst_files_size);
+            tablet_metadata->add_replicas_wal_files_size(replica.drive_info.wal_files_size);
           }
         }
       }
@@ -14304,6 +14310,8 @@ Status CatalogManager::GetTabletsMetadata(const GetTabletsMetadataRequestPB* req
       // Add leader as the last replica
       if (!leader_address.empty()) {
         tablet_metadata->add_replicas(leader_address);
+        tablet_metadata->add_replicas_sst_files_size(leader_sst_files_size);
+        tablet_metadata->add_replicas_wal_files_size(leader_wal_files_size);
       }
 
       // last_status is a required field in the PB
